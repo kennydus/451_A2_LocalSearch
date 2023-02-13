74a60c70e846a221efadf467c709f9e25bfbf817
Kenny Du <61264046+kennydus@users.noreply.github.com>
2023-02-13 15:07:58 -0800

    Notes for hill

---
+++ hill.py
@@ -0,0 +1,12 @@
+# initialize variables
+#   fitness (h?)
+#   best_index
+#   current_fitness = inf
+
+# start at row 1
+#   put row 1's queen in other spots in same row (first find where the queen is)
+#       do this by using the flip function
+#       check fitness for each run
+#           if fitness < current_fitness: change best_index
+
+#
