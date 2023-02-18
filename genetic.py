73a8c88f84d840750a9ba6e72b9e5033eda5a48f
Kenny Du <61264046+kennydus@users.noreply.github.com>
2023-02-17 20:03:36 -0800

    Reformatted files to have board.py be in its original form.

--- genetic.py
+++ genetic.py
@@ -1,24 +1,52 @@
 import math
 import random
 import time
+import board
+
+
+# Remake the boards so that there are only 1 queen per column.
+def remake_board(in_board):
+    in_board.map = [[0 for _ in range(in_board.n_queen)] for _ in range(in_board.n_queen)]
+    for column in range(in_board.n_queen):
+        row = random.randint(0, in_board.n_queen - 1)
+        in_board.map[row][column] = 1
+    return in_board
+
+
+# Returns number of attacking pairs.
+# Starting from leftmost column, checks for queens in the
+# diagonal and horizontal planes towards the right side.
+def get_genetic_fitness(in_board):
+    fit = 0
+    for i in range(in_board.n_queen):
+        for j in range(in_board.n_queen):
+            if in_board.map[j][i] == 1:
+                for k in range(1, in_board.n_queen - i):
+                    if in_board.map[j][i + k] == 1:  # checking horizontal (right side)
+                        fit += 1
+                    if j - k >= 0 and in_board.map[j - k][i + k] == 1:  # checking diagonal (up + right)
+                        fit += 1
+                    if j + k < in_board.n_queen and in_board.map[j + k][i + k] == 1:  # checking diagonal (down + right)
+                        fit += 1
+    return fit
 
 
 # Prints out the completed board
-def output_board(board):
-    for i in range(board.n_queen):
-        for j in range(board.n_queen):
-            if board.map[i][j] == 1:
+def output_board(in_board):
+    for i in range(in_board.n_queen):
+        for j in range(in_board.n_queen):
+            if in_board.map[i][j] == 1:
                 print(1, end=' ')
             else:
                 print('- ', end='')
-        print('\n')
+        print()
 
 
-def generate_q_string(board):
+def generate_q_string(in_board):
     q_string = ''
-    for column in range(board.n_queen):
-        for row in range(board.n_queen):
-            if board.map[row][column] == 1:
+    for column in range(in_board.n_queen):
+        for row in range(in_board.n_queen):
+            if in_board.map[row][column] == 1:
                 q_string += str(row + 1)
                 break
     return q_string
@@ -26,11 +54,17 @@ def generate_q_string(board):
 
 def genetic(boards):
     start_time = time.time_ns()
+    n_queen = boards[0].n_queen
     iteration = 0
     # FOR EACH ARRAY, VALUES BELONG TO EACH BOARD IN ORDER
     # EX. initial_population[0] contains the string of queen positions for board #1.
 
-    total_pairs = math.comb(boards[0].n_queen, 2)
+    # Remake the boards so that there are only 1 queen per column.
+    for i in range(len(boards)):
+        boards[i] = remake_board(boards[i])
+
+    # Maximum amount of non-attacking pairs
+    total_pairs = math.comb(n_queen, 2)
 
     while True:
         # Array of strings (digits) that indicate the position of the queen in each column, per board.
@@ -38,13 +72,13 @@ def genetic(boards):
         # Number of non-attacking pairs per board
         non_attacking_pairs = []
 
-        for board in boards:
+        for b in boards:
             # q_string represents the position of the queens in each column, starting from the leftmost column.
-            # Top-most row is '1', and bottom-most row is '5'.
-            q_string = generate_q_string(board)
+            # Top-most row is '1', and bottom-most row is 'n_queen'.
+            q_string = generate_q_string(b)
             initial_population.append(q_string)
 
-            non_attacking_pairs.append(total_pairs - board.get_genetic_fitness())
+            non_attacking_pairs.append(total_pairs - get_genetic_fitness(b))
 
         # Probability of each board being chosen for reproducing.
         choose_prob = []
@@ -76,7 +110,7 @@ def genetic(boards):
         children = []
         for i in range(0, len(boards), 2):
             # Crossover point should include at least 1 digit from each parent.
-            cross_point = random.randint(1, 3)
+            cross_point = random.randint(1, n_queen - 2)
             children.append(parents[i][:cross_point] + parents[i + 1][cross_point:])
             children.append(parents[i + 1][:cross_point] + parents[i][cross_point:])
 
@@ -85,8 +119,8 @@ def genetic(boards):
             # Mutation: We will set chance of mutation to be 10%
             if random.random() < .1:
                 # Getting index of queen to be mutated
-                mut_index = random.randint(0, 4)
-                mut_position = random.randint(1, 5)
+                mut_index = random.randint(0, n_queen - 1)
+                mut_position = random.randint(1, n_queen)
 
                 # print(f'q_string mutated: {children[index]} at index {mut_index}')
                 mutated_children.append(
@@ -95,8 +129,8 @@ def genetic(boards):
                 mutated_children.append(children[index])
 
         # Reset boards to zero, and create ones that match the strings from mutation list.
-        for board in boards:
-            board.map = [[0 for _ in range(5)] for _ in range(5)]
+        for b in boards:
+            b.map = [[0 for _ in range(n_queen)] for _ in range(n_queen)]
         for i in range(len(boards)):
             current_column = 0
             for q_pos in mutated_children[i]:
@@ -105,24 +139,21 @@ def genetic(boards):
 
         iteration += 1
 
-
-        print('initial_population:\t', initial_population)
-        print('choose_prob:\t\t', end=' ')
-        rounded_probs = []
-        for prob in choose_prob:
-            rounded_probs.append('{:.3f}'.format(round(prob, 5)))
-        print(rounded_probs)
-
-        print(f'non-attack pairs:\t {non_attacking_pairs}')
-        print('parents:\t\t\t', parents)
-        print('children:\t\t\t', children)
-        print(f'mutated_children:\t {mutated_children}\n')
-
         # Test fitness of each board
         for i in range(len(boards)):
-            if boards[i].get_genetic_fitness() == 0:
+            if get_genetic_fitness(boards[i]) == 0:
                 running_time = (time.time_ns() - start_time) / 10 ** 6
-                print('Generations:', iteration)
+                # print('Generations:', iteration)
                 print(f'Running time: {round(running_time)}ms')
                 output_board(boards[i])
                 return boards[i]
+
+
+if __name__ == '__main__':
+    board_array = []
+    for i in range(8):
+        temp = board.Board(5)
+        board_array.append(temp)
+
+    # print('=== Running Genetic ===')
+    genetic(board_array)
--- hill.py
+++ hill.py
@@ -1,49 +1,59 @@
 import time
+import board
 
 
 # Prints out the completed board
-def output_board(board):
-    for i in range(board.n_queen):
-        for j in range(board.n_queen):
-            if board.map[i][j] == 1:
+def output_board(in_board):
+    for i in range(in_board.n_queen):
+        for j in range(in_board.n_queen):
+            if in_board.map[i][j] == 1:
                 print(1, end=' ')
             else:
                 print('- ', end='')
-        print('\n')
+        print()
 
 
-def hill_climb(board):
+def hill_climb(in_board):
     start_time = time.time_ns()
     # Initializing variables
-    best_fitness = board.get_fitness()
+    best_fitness = in_board.get_fitness()
+
+    n_queen = in_board.n_queen
 
     while best_fitness != 0:
-        for row in range(board.n_queen):
+        for row in range(n_queen):
             # Represents index of queen in the row that results in the least number of attacking pairs.
             # Set it to the row's initial queen's index.
-            best_index = board.map[row].index(1)
-            best_fitness = board.get_fitness()
+            best_index = in_board.map[row].index(1)
+            best_fitness = in_board.get_fitness()
 
-            board.flip(row, board.map[row].index(1))  # Flip the current row's queen to 0
-            for spot in range(board.n_queen):
+            in_board.flip(row, in_board.map[row].index(1))  # Flip the current row's queen to 0
+            for spot in range(n_queen):
                 # Set current spot to be a queen
-                board.flip(row, spot)
-                fitness = board.get_fitness()
+                in_board.flip(row, spot)
+                fitness = in_board.get_fitness()
                 if fitness < best_fitness:
                     best_fitness = fitness
                     best_index = spot
                 # Remove queen from current spot
-                board.flip(row, spot)
+                in_board.flip(row, spot)
             # Set the queen at the best_index
-            board.flip(row, best_index)
+            in_board.flip(row, best_index)
 
         # Reset the board (random restart)
         if best_fitness != 0:
-            board.__init__(board.n_queen)
+            in_board.__init__(n_queen)
 
     # Convert running time from ns to milliseconds.
     running_time = (time.time_ns() - start_time) / (10 ** 6)
     print(f'Running time: {round(running_time)} ms')
 
     # Printing out the completed board
-    output_board(board)
+    output_board(in_board)
+
+
+if __name__ == '__main__':
+    temp = board.Board(5)
+
+    # print('=== Running Hill-Climb Algorithm ===')
+    hill_climb(temp)
